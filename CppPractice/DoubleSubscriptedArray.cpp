3766bbcc189b12721a217bdeebd4116d78c5ae7c
cheongisabella <cheongisabella@gmail.com>
2023-03-06 18:18:34 +0800

    second

--- CppPractice/DoubleSubscriptedArray.cpp
+++ CppPractice/DoubleSubscriptedArray.cpp
@@ -1,8 +1,31 @@
 #include "DoubleSubscriptedArray.h"
+#include <stdexcept>
 using namespace std;
 
-DoubleSubscriptedArray::DoubleSubscriptedArray(int r, int c) {
+//default constructor for class DoubleSubscriptedArray (default size 1)
+DoubleSubscriptedArray::DoubleSubscriptedArray(int numRow, int numCol)
+	: size( (numRow > 0 && numCol > 0) ? (numRow * numCol) : 
+		throw invalid_argument("Both the number of rows and columns must be greater than 0")),
+	ptr(new int[size])
+{
+	for (size_t i = 0; i < size; ++i) {
+		ptr[i] = 0;
+	}
+}
 
+//copy constructor for class DoubleSubscriptedArray
+DoubleSubscriptedArray::DoubleSubscriptedArray(const DoubleSubscriptedArray& arrayToCopy)
+	: size(arrayToCopy.size),
+	ptr(new int[size])
+{
+	for (size_t i = 0; i < size; ++i) {
+		ptr[i] = arrayToCopy.ptr[i];
+	}
+}
+
+//destructor for class DoubleSubscriptedArray
+DoubleSubscriptedArray::~DoubleSubscriptedArray() {
+	delete [] ptr; //release pointer-based array space
 }
 
 int& DoubleSubscriptedArray::operator()(int r, int c) {
