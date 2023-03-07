13d289b6e29349979514fa3f808ec0ecf2c2bf41
cheongisabella <cheongisabella@gmail.com>
2023-03-07 18:10:32 +0800

    ch18ex7 done

--- CppPractice/DoubleSubscriptedArray.h
+++ CppPractice/DoubleSubscriptedArray.h
@@ -1,10 +1,40 @@
+#ifndef TEST_CH18EX7
 #pragma once
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <iostream>
+
 class DoubleSubscriptedArray {
+
+	friend std::ostream& operator<<(std::ostream &, const DoubleSubscriptedArray &);
+	friend std::istream& operator>>(std::istream&, DoubleSubscriptedArray&);
+
 public:
-	explicit DoubleSubscriptedArray(int = 1, int = 1);
+	explicit DoubleSubscriptedArray(int = 2, int = 2);
+	DoubleSubscriptedArray(const DoubleSubscriptedArray &); //copy constructor 
+	~DoubleSubscriptedArray(); //destructor
+	size_t getSize() const;
 
-	int& operator()(int, int); //return element of double-subscripted array to be used as modifiable lvalue
-	int operator()(int, int) const; //return element of double-subscripted array as rvalue
+	const DoubleSubscriptedArray& operator=(const DoubleSubscriptedArray &); //assignment operator 
+	bool operator==(const DoubleSubscriptedArray&) const; //equality operator
+	//inequality operator, returns opposite of == operator
+	bool operator!=(const DoubleSubscriptedArray &right) const {
+		return !(*this == right); //invokes DoubleSubscriptedArray::operator==
+	} 
+
+	//double subcript operator for non-const objects returns modifiable lvalue
+	int& operator()(int, int); 
+
+	//double subscript operator for const objects returns non-modifiable rvalue
+	int operator()(int, int) const; 
+
 private:
-
+	size_t size;
+	int* ptr;
+	int row;
+	int col;
 };
+
+#endif
+#endif
--- CppPractice/ch17ex20.cpp
+++ CppPractice/ch17ex20.cpp
@@ -1,5 +1,34 @@
+#ifdef TEST_CH17EX20
 #include "SavingsAccount.h"
+#include <iostream>
 
 int main() {
 
+	std::cout << "Before creating any objects of class, static variable annualInterestRate = "
+		<< SavingsAccount::getInterest() << std::endl;
+
+	SavingsAccount saver0;
+	saver0.printAccount();
+
+	SavingsAccount saver1(2000.00);
+	SavingsAccount saver2(3000.00);
+	saver1.printAccount();
+	saver2.printAccount();
+
+	saver1.modifyInterestRate(3);
+	saver1.printAccount();
+	saver2.printAccount();
+
+	saver1.calculateMonthlyInterest();
+	saver2.calculateMonthlyInterest();
+	saver1.printAccount();
+	saver2.printAccount();
+
+	saver2.modifyInterestRate(4);
+	saver1.calculateMonthlyInterest();
+	saver2.calculateMonthlyInterest();
+	saver1.printAccount();
+	saver2.printAccount();
 }
+
+#endif
