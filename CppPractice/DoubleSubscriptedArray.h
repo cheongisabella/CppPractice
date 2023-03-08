bc328411af6ec08592dfc3cfeec704022dacf2d1
cheongisabella <cheongisabella@gmail.com>
2023-03-08 11:04:18 +0800

    stashed changes

--- CppPractice/DoubleSubscriptedArray.h
+++ CppPractice/DoubleSubscriptedArray.h
@@ -2,9 +2,11 @@
 class DoubleSubscriptedArray {
 public:
 	explicit DoubleSubscriptedArray(int = 1, int = 1);
-
+	DoubleSubscriptedArray(const DoubleSubscriptedArray &);
+	~DoubleSubscriptedArray(); //destructor
 	int& operator()(int, int); //return element of double-subscripted array to be used as modifiable lvalue
 	int operator()(int, int) const; //return element of double-subscripted array as rvalue
 private:
-
+	int size;
+	int* ptr;
 };
